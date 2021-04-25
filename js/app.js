dcb4ec6cd85dd20e7925ba7591c872c89c7e4517
suadtalafha <suadhusam@yahoo.com>
2021-04-25 15:59:21 +0300

    save

---
+++ js/app.js
@@ -0,0 +1,24 @@
+let username=prompt("Please type your name")
+console.log(username)
+alert("welcome to our site "+username )
+
+let userage=prompt("Please type your age")
+console.log(userage)
+alert("Cool")
+
+
+let usertype=prompt("Do you want dress or shirts?")
+console.log(usertype)
+alert("Good choise")
+
+let userprefer=prompt("Please give us your comment")
+console.log(userprefer)
+alert("Thank you")
+
+document.write('welcome to our site '+ username);
+
+document.write( 'Your age  ' + userage);
+
+document.write('You want '+ usertype );
+
+document.write('Your comment '+ userprfer);
